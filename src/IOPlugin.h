262504f7185e2dbaa393601142ca341c9164d87d
Thomas Egger <thoemsi@gmail.com>
2010-01-12 11:20:45 +0000

    added comments to IOPlugin - the helper class for registering plugins in class IOHandler

--- src/IOPlugin.h
+++ src/IOPlugin.h
@@ -21,13 +21,32 @@
 #include "DynamicLibrary.h"
 #include "IOInterface.h"
 
+/**
+ * @class IOPlugin
+ * @brief A helper class representing a MeteoIO plugin in the context of the dynamic loading as implemented in class 
+ * 	IOHandler. Each IOPlugin object represents an implementation of IOInterface or in more tangible terms it represents
+ *	an instance of IOInterface in use. The two pointer member variables *io and *dynLibrary represent the opened
+ *	dynamic library and the loaded object (from within that library). These pointers are essential for knowing
+ *	how to deallocate the loaded object and library. 
+ *
+ * @author Thomas Egger
+ * @date   2009-08-11
+ */
 class IOPlugin {
 	public:
-		std::string libname;
-		std::string classname;
-		IOInterface *io;
-		DynamicLibrary *dynLibrary;
+		std::string libname; ///< A string representing the file to be loaded, e.g. "libgeotopio.so", can be empty
+		std::string classname; ///< A short name for the plugin (unique!), e.g. "A3DIO", "GSNIO"
+		IOInterface *io; ///< The pointer to the actual dynamically loaded instance of IOInterface
+		DynamicLibrary *dynLibrary; ///< The pointer to the opened dynamic library
 		
+		/**
+		 * @brief The main constructor for the IOPlugin class
+		 *
+		 * @param _s1 A std::string representing the file to be opened (or "" if plugin is statically linked)
+		 * @param _s2 A std::string to identify the plugin uniquely (should be something like "A3DIO")
+		 * @param p1  A pointer to the loaded object of type IOInterface (or NULL)
+		 * @param p2  A pointer to the loaded dynamic library (or NULL)
+		 */
 		IOPlugin(std::string _s1, std::string _s2, IOInterface *p1, DynamicLibrary *p2) : libname(_s1), classname(_s2), io(p1), dynLibrary(p2){}
 		IOPlugin() : libname(""), classname(""), io(NULL), dynLibrary(NULL){}
 };
