750b939f31c4b2c7b2efea008018720959c0cf1a
Mathias Bavay <bavay@slf.ch>
2010-04-06 16:21:29 +0000

    The example io.ini has been brought up to date, a new example showing how to perform spatial interpolations has been added.

---
+++ doc/examples/2D_interpolations.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include "MeteoIO.h"
+
+//This is the a basic example of spatial interpolations. It does not check any exceptions, it only tries to be as c-like as possible
+//provide date as ISO formatted, for example 2008-12-01T15:35:00 and 
+//it will retrieve and interpolate the data for this date according to the io.ini configuration file
+int main(int argc, char** argv) {
+	(void)argc;
+
+	Date_IO d1;
+	std::vector<MeteoData> vecMeteo;
+	std::vector<StationData> vecStation;
+
+	//initializing the io handlers according to the config file
+	ConfigReader cfg("io.ini");
+	IOHandler *raw_io = NULL;
+	BufferedIOHandler *io = NULL;
+	raw_io = new IOHandler(cfg);
+	io = new BufferedIOHandler(*raw_io, cfg);
+
+	//reading the dem (necessary for several spatial interpolations algoritms)
+	DEMObject dem;
+	io->readDEM(dem);
+
+	//reading the meteorological data for the requested time step
+	IOUtils::convertString(d1,argv[1]);
+	io->readMeteoData(d1, vecMeteo, vecStation);
+
+	//performing spatial interpolations
+	Meteo2DInterpolator mi(cfg, dem, vecMeteo, vecStation);
+	Grid2DObject ta;
+	mi.interpolate(MeteoData::TA, ta);
+
+	//writing out the spatial interpolation
+	io->write2DGrid(ta,"ta.asc");
+
+	delete io;
+	delete raw_io;
+	return 0;
+}
