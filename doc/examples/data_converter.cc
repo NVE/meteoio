bf2860e3a31c83295abcc8ecab483ec3a21e5f6c
Mathias Bavay <bavay@slf.ch>
2012-06-18 10:29:15 +0000

    Cleanup in the documentation (with a better data_converter) and fixes for XCode 4.4 on osX

--- doc/examples/data_converter.cc
+++ doc/examples/data_converter.cc
@@ -3,27 +3,25 @@
 
 using namespace mio; //The MeteoIO namespace is called mio
 
-//This is the most basic example. It does not check any exceptions, it only tries to be as c-like as possible
-//provide date as ISO formatted, for example 2008-12-01T15:35:00 and 
-//it will retrieve the data for this date according to the io.ini configuration file
+//This example takes two ISO-formatted dates on the command line
+//for example ./data_converter 2008-12-01T00:00:00 2008-12-31T23:00
+//It will retrieve the data for this time interval and write it out as specified
+//in the io.ini configuration
 int main(int argc, char** argv) {
-	if(argc==1 || argc>3) {
-		std::cout << "Invalid number of arguments!\n";
+	if(argc!=2) {
+		std::cout << "Invalid number of arguments! Please provide a date range!\n";
 		exit(0);
 	}
 
-	Date d1, d2;
-	IOUtils::convertString(d1,argv[1], 1.);
-	if(argc==3)
-		IOUtils::convertString(d2,argv[2], 1.);
-	else
-		d2.setFromSys();
-
 	Config cfg("io.ini");
+	Date d1, d2;
+	const double TZ = cfg.get("Input", "TIME_ZONE");
+	IOUtils::convertString(d1,argv[1], TZ);
+	IOUtils::convertString(d2,argv[2], TZ);
+
+	std::vector< std::vector<MeteoData> > vecMeteo;
 	IOManager io(cfg);
 	//io.setProcessingLevel(IOManager::raw);
-	std::vector< std::vector<MeteoData> > vecMeteo;
-
 	std::cout << "Reading input data" << std::endl;
 
 	//Very basic conversion: get the whole data set at once, with its original sampling rate
@@ -34,7 +32,7 @@ int main(int argc, char** argv) {
 	std::vector<MeteoData> Meteo; //we need some intermediate storage, for storing data sets for 1 timestep
 	io.getMeteoData(d1, Meteo); //we need to know how many stations will be available
 	vecMeteo.insert(vecMeteo.begin(), Meteo.size(), std::vector<MeteoData>()); //allocation for the vectors
-	for(; d1<=d2; d1+=.5/24.) { //time loop, sampling rate = 1/24 day = 1 hour
+	for(; d1<=d2; d1+=1./24.) { //time loop, sampling rate = 1/24 day = 1 hour
 		io.getMeteoData(d1, Meteo); //read 1 timestep at once, forcing resampling to the timestep
 		for(unsigned int ii=0; ii<Meteo.size(); ii++) {
 			vecMeteo.at(ii).push_back(Meteo[ii]); //fill the data manually into the vector of vectors
