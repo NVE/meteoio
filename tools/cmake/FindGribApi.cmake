baae08c77cf38efd31d8b929c8f4d0cdc32e9443
Mathias Bavay <bavay@slf.ch>
2013-10-30 12:29:03 +0000

    Better detection of Grib_API

--- tools/cmake/FindGribApi.cmake
+++ tools/cmake/FindGribApi.cmake
@@ -12,11 +12,17 @@ PATHS
 DOC "Location of the grib-api library, like /usr/lib/libgrib_api.so"
 )
 
+#build GRIBAPI_ROOT so we can provide a hint for searching for the header file
+GET_FILENAME_COMPONENT(grib_api_libs_root ${GribApi_LIBRARY} PATH)
+SET(GRIBAPI_ROOT "${meteoio_libs_root}/../")
+STRING(REPLACE  " " "\\ " GRIBAPI_ROOT ${GRIBAPI_ROOT})
+
 # locate main header file
 FIND_PATH(GribApi_INCLUDE_DIR
   NAMES grib_api.h
-  #HINTS ${METEOIO_ROOT}/include
+  #HINTS ${GRIBAPI_ROOT}/include
   PATHS
+	"${GRIBAPI_ROOT}/include"
 	"~/usr/include"
 	"/usr/local/include"
 	"/usr/include"
