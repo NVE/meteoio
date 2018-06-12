3d308b961ea9c6eced26b2dd6cde1dfca760cb71
Mathias Bavay <bavay@slf.ch>
2018-06-12 09:23:29 +0000

    Crocus needs split precipitation and from the measurements data, there is often only a precipitation amount. It is then necessary to compute the splitting and then distribute it to split precipitation. This has all be integrated into the old PPHASE generator that has been renamed as PrecSplitting since it can now generate the phase and/or convert between amount/phase and PSUM_L/PSUM_S.

--- meteoio/dataGenerators/PrecSplitting.h
+++ meteoio/dataGenerators/PrecSplitting.h
@@ -1,5 +1,5 @@
 /***********************************************************************************/
-/*  Copyright 2013 WSL Institute for Snow and Avalanche Research    SLF-DAVOS      */
+/*  Copyright 2013-2018 WSL Institute for Snow and Avalanche Research    SLF-DAVOS */
 /***********************************************************************************/
 /* This file is part of MeteoIO.
     MeteoIO is free software: you can redistribute it and/or modify
@@ -15,18 +15,25 @@
     You should have received a copy of the GNU Lesser General Public License
     along with MeteoIO.  If not, see <http://www.gnu.org/licenses/>.
 */
-#ifndef PPHASEGENERATOR_H
-#define PPHASEGENERATOR_H
+#ifndef PRECSPLITTING_H
+#define PRECSPLITTING_H
 
 #include <meteoio/dataGenerators/GeneratorAlgorithms.h>
 
 namespace mio {
 
 /**
- * @class PPhaseGenerator
+ * @class PrecSplitting
  * @ingroup parametrizations
- * @brief Generate precipitation splitting according to the selected method
+ * @brief Generate precipitation phase or splitting according to the selected method
  * @details
+ * In order to handle solid and liquid precipitation, there are two possibilities: either as total amount and phase (respectively "PSUM" and "PSUM_PH")
+ * or as solid and liquid amounts (respectively "PSUM_S" and "PSUM_L"). This generator can convert from one representation to the other one or 
+ * generate the missing components (for example, the precipitation phase associated with a given precipitation amout based on a splitting model).
+ * 
+ * The component that will be generated depends on the parameter name, so only the following parameters are supported: PSUM, PSUM_PH, PSUM_L, PSUM_S.
+ * Whenever a component is missing, a splitting model is called to compute the splitting. It is therefore mandatory to configure a splitting model.
+ * 
  * It takes the following arguments:
  *  - TYPE: the splitting method to use, any of the following:
  *     - THRESH: a provided fixed air temperature threshold splits precipitation as either fully solid or fully liquid
@@ -36,16 +43,31 @@ namespace mio {
  * thresholds, this provides the temperature below which only solid precipitation is found (in K);
  *  - RAIN: when using two air temperatures thresholds, this provides the temperature above which only liquid precipitation is found (in K);
  *
+ * For example, to generate the precipitation phase when only the precipitation amout is available, using a simple temperature
+ * threshold for doing the splitting:
  * @code
- * PSUM_PH::generators     = PPHASE
+ * PSUM_PH::generators     = PRECSPLITTING
  * PSUM_PH::PPHASE::type   = THRESH
  * PSUM_PH::PPHASE::snow   = 274.35
  * @endcode
+ * 
+ * To generate the liquid and solid amounts from the precipitation sum, relying on a simple temperature threshold for the splitting
+ * (if the precipitation phase is available, it will be used instead of calling the splitting model):
+ * @code
+ * PSUM_L::create     = PPHASE
+ * PSUM_L::PPHASE::type   = THRESH
+ * PSUM_L::PPHASE::snow   = 274.35
+ * 
+ * PSUM_S::create     = PPHASE
+ * PSUM_S::PPHASE::type   = THRESH
+ * PSUM_S::PPHASE::snow   = 274.35
+ * @endcode
+ * 
  */
-class PPhaseGenerator : public GeneratorAlgorithm {
+class PrecSplitting : public GeneratorAlgorithm {
 	public:
-		PPhaseGenerator(const std::vector< std::pair<std::string, std::string> >& vecArgs, const std::string& i_algo)
-			: GeneratorAlgorithm(vecArgs, i_algo), model(THRESH), fixed_thresh(IOUtils::nodata),
+		PrecSplitting(const std::vector< std::pair<std::string, std::string> >& vecArgs, const std::string& i_algo)
+			: GeneratorAlgorithm(vecArgs, i_algo), model(THRESH), where("generators::"+algo), fixed_thresh(IOUtils::nodata),
 			range_start(IOUtils::nodata), range_norm(IOUtils::nodata) { parse_args(vecArgs); }
 
 		bool generate(const size_t& param, MeteoData& md);
@@ -53,12 +75,18 @@ class PPhaseGenerator : public GeneratorAlgorithm {
 
 	private:
 		void parse_args(const std::vector< std::pair<std::string, std::string> >& vecArgs);
+		bool generatePSUM_S(double& value, MeteoData& md) const;
+		bool generatePSUM_L(double& value, MeteoData& md) const;
+		bool generatePSUM_PH(double& value, MeteoData& md) const;
+		bool generatePSUM(double& value, MeteoData& md) const;
+		bool runModel(double &value, MeteoData& md) const;
 
 		typedef enum PARAMETRIZATION {
 			THRESH,
 			RANGE
 		} parametrization;
 		parametrization model;
+		const std::string where;
 		double fixed_thresh, range_start, range_norm;
 };
 
