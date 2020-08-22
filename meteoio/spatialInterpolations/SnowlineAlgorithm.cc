9f8227fd1c3c0187d57b5dcd7ffaaee569c301df
Michael Reisecker <m.r@sbg.at>
2020-08-22 16:00:07 +0000

    Adding sketch for new 2D interpolation. Goal is to assimilate a known snowline elevation in easy ways. Will not be documented until next release.

---
+++ meteoio/spatialInterpolations/SnowlineAlgorithm.cc
@@ -0,0 +1,167 @@
+/***********************************************************************************/
+/*  Copyright 2020 ALPsolut.eu                                                     */
+/***********************************************************************************/
+/* This file is part of MeteoIO.
+    MeteoIO is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    MeteoIO is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with MeteoIO.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <meteoio/FileUtils.h>
+#include <meteoio/meteoStats/libinterpol2D.h>
+#include <meteoio/spatialInterpolations/SnowlineAlgorithm.h>   
+
+#include <fstream>
+#include <limits>
+
+namespace mio {
+
+SnowlineAlgorithm::SnowlineAlgorithm(const std::vector< std::pair<std::string, std::string> >& vecArgs,
+    const std::string& i_algo, const std::string& i_param, TimeSeriesManager& i_tsm,
+    GridsManager& i_gdm, Meteo2DInterpolator& i_mi) : 
+    InterpolationAlgorithm(vecArgs, i_algo, i_param, i_tsm), gdm(i_gdm), mi(i_mi), base_alg("IDW_LAPSE"),
+    snowline(IOUtils::nodata), assim_method(CUTOFF), snowline_file(), where("Interpolations2D::" + algo),
+    quiet(false)
+{
+	for (size_t ii = 0; ii < vecArgs.size(); ii++) {
+		if (vecArgs[ii].first == "BASE") {
+			base_alg = IOUtils::strToUpper(vecArgs[ii].second);
+		} else if (vecArgs[ii].first == "SNOWLINE") {
+			IOUtils::parseArg(vecArgs[ii], where, snowline);
+		} else if (vecArgs[ii].first == "SNOWLINEFILE") {
+			snowline_file = vecArgs[ii].second;
+		} else if (vecArgs[ii].first == "MODE") {
+			const std::string mode( IOUtils::strToUpper(vecArgs[ii].second) );
+			if (mode == "CUTOFF")
+				assim_method = CUTOFF;
+			else
+				throw InvalidArgumentException("Snowline assimilation mode \"" + mode +
+				    "\" supplied for " + where + " not known.", AT);
+		} else if (vecArgs[ii].first == "QUIET") {
+			IOUtils::parseArg(vecArgs[ii], where, quiet);
+		}
+	}
+}
+
+double SnowlineAlgorithm::getQualityRating(const Date& i_date)
+{
+	date = i_date; //this is always called before calculate() and sets the date!
+	nrOfMeasurments = getData(date, param, vecData, vecMeta); //more initialization
+	if (nrOfMeasurments == 0)
+		return 0.0;
+	if (snowline != IOUtils::nodata) //TODO: possible to propagate from base alg?
+		return 0.8;
+	else
+		return 0.7;
+}
+
+void SnowlineAlgorithm::calculate(const DEMObject& dem, Grid2DObject& grid)
+{
+	getSnowline();
+	if (snowline == IOUtils::nodata) //we already gave notice for this
+		return; 
+
+	if (assim_method == CUTOFF) {
+		baseInterpol(dem, grid);
+		assimilateCutoff(dem, grid);
+	}
+}
+
+void SnowlineAlgorithm::baseInterpol(const DEMObject& dem, Grid2DObject& grid)
+{
+	std::string base_alg_fallback( base_alg ); //fallback algorithm if only 1 station is used
+	if (nrOfMeasurments == 1) {
+		base_alg_fallback = "AVG";
+		msg("[W] Falling back to \"AVG\" for " + where + " (insufficient number of stations on " +
+		    date.toString(Date::ISO_DATE) + ").");
+	}
+	//flexibly read parameters for base algorithm:
+	const std::vector< std::pair<std::string, std::string> >
+	    vecArgs( mi.getArgumentsForAlgorithm(param, base_alg_fallback, "Interpolations2D") );
+	InterpolationAlgorithm* algorithm(
+	    AlgorithmFactory::getAlgorithm(base_alg_fallback, mi, vecArgs, tsmanager, gdm, param) );
+	//apply base interpolation algorithm to whole grid:
+	algorithm->getQualityRating(date); //set date
+	algorithm->calculate(dem, grid);
+	info << algorithm->getInfo();
+	delete algorithm;
+}
+
+void SnowlineAlgorithm::assimilateCutoff(const DEMObject& dem, Grid2DObject& grid)
+{ //set everything below snowlin elevation to zero
+	for (size_t ii = 0; ii < grid.getNx(); ii++) {
+		for (size_t jj = 0; jj < grid.getNy(); jj++) {
+			if (dem(ii, jj) == IOUtils::nodata)
+				continue;
+			if (dem(ii, jj) < snowline)
+				grid(ii, jj) = 0.;
+		}
+	}
+}
+
+double SnowlineAlgorithm::readSnowlineFile()
+{
+	std::ifstream fin( snowline_file.c_str() );
+	if (fin.fail())
+		return IOUtils::nodata;
+	
+	double snowline_elevation;
+	const char eoln = FileUtils::getEoln(fin); //get the end of line character for the file
+	try {
+		do {
+			std::string line;
+			getline(fin, line, eoln);
+			IOUtils::stripComments(line);
+			IOUtils::trim(line);
+			if (line.empty()) continue; //skip comments
+
+			std::istringstream iss( line );
+			iss.setf(std::ios::fixed);
+			iss.precision(std::numeric_limits<double>::digits10);
+			iss >> std::skipws >> snowline_elevation;
+			if (!iss) 
+				return IOUtils::nodata;
+		} while (!fin.eof());
+		fin.close();
+	} catch (const std::exception&){
+		if (fin.is_open()) fin.close();
+		throw;
+	}
+	return snowline_elevation;
+}
+
+void SnowlineAlgorithm::getSnowline() 
+{
+	if (!snowline_file.empty()) {
+		if (snowline != IOUtils::nodata) {
+			msg("[i] Ignoring additional SNOWLINE argument since SNOWFILE is given for " + where + ".");
+		}
+		snowline = readSnowlineFile();
+		if (snowline == IOUtils::nodata) {
+			msg("[W] No valid snowline elevation can be read from SNOWFILE for " +
+			    where + ". Continuing without...");
+		}
+	} else {
+		if (snowline == IOUtils::nodata) {
+			msg("[W] No numeric value found for SNOWLINE, no SNOWFILE provided either for " +
+			    where + ". Continuing without...");
+		}
+	}
+}
+
+void SnowlineAlgorithm::msg(const std::string& message)
+{
+	if (!quiet)
+		std::cerr << message << std::endl;
+}
+
+} //namespace
---
+++ meteoio/spatialInterpolations/SnowlineAlgorithm.h
@@ -0,0 +1,59 @@
+/***********************************************************************************/
+/*  Copyright 2020 ALPsolut.eu                                                     */
+/***********************************************************************************/
+/* This file is part of MeteoIO.
+    MeteoIO is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    MeteoIO is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with MeteoIO.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef SNOWLINE_ALGORITHM_H
+#define SNOWLINE_ALGORITHM_H
+
+#include <string>
+
+#include <meteoio/spatialInterpolations/InterpolationAlgorithms.h>
+
+namespace mio {
+
+class SnowlineAlgorithm : public InterpolationAlgorithm {
+	public:
+		SnowlineAlgorithm(const std::vector< std::pair<std::string, std::string> >& vecArgs,
+		    const std::string& i_algo, const std::string& i_param, TimeSeriesManager& i_tsm,
+		    GridsManager& i_gdm, Meteo2DInterpolator& i_mi);
+		virtual double getQualityRating(const Date& i_date);
+		virtual void calculate(const DEMObject& dem, Grid2DObject& grid);
+
+	private:
+		typedef enum METHOD //assimilation method
+		{
+			CUTOFF //!< set all below snowline to zero
+		} assimilation_method;
+
+		void baseInterpol(const DEMObject& dem, Grid2DObject& grid);
+		void assimilateCutoff(const DEMObject& dem, Grid2DObject& grid);
+		double readSnowlineFile();
+		void getSnowline();
+		void msg(const std::string& message);
+
+		GridsManager& gdm;
+		Meteo2DInterpolator& mi;
+		std::string base_alg;
+		double snowline;
+		assimilation_method assim_method;
+		std::string snowline_file;
+		std::string where;
+		bool quiet; //suppress warnings
+};
+
+} //end namespace mio
+
+#endif //SNOWLINE_ALGORITHM_H
