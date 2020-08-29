f6ca32a4b3ad6f90b80078f24738289c0c50db44
Nander Wever <Nander.Wever@colorado.edu>
2020-08-29 01:03:12 +0000

    Introducing a universal function to convert wind speed components to wind direction, and vice versa, with additional corrections for wind component calculations.

--- meteoio/IOUtils.h
+++ meteoio/IOUtils.h
@@ -82,7 +82,32 @@ namespace IOUtils {
 
 	inline double C_TO_K(const double& T) {return ((T==nodata)? T : T + Cst::t_water_freezing_pt);}
 	inline double K_TO_C(const double& T) {return ((T==nodata)? T : T - Cst::t_water_freezing_pt);}
-	
+
+	/**
+	* @brief From wind speed components (u,v) to wind direction, following standard meteorological definitions:
+	* U is positive with positive easting/longitude. V is positive with positive northing/latitude. Direction is the angle the wind comes from.
+	* @param U (west-to-east component)
+	* @param V (south-to-north component)
+	* @return Wind direction in degrees, defined as the direction the wind is coming from.
+	*/
+	inline double UV_TO_DW(const double& U, const double& V) {return ((U==nodata || V==nodata)? nodata : fmod(atan2(U, V) * Cst::to_deg + 180., 360.));}
+
+	/**
+	* @brief From wind speed and direction to u component (west-to-east)
+	* @param VW (wind speed)
+	* @param DW (wind direction)
+	* @return U component of wind.
+	*/
+	inline double VWDW_TO_U(const double& VW, const double& DW) {return ((VW==nodata || DW==nodata)? nodata : (- VW * sin(DW*Cst::to_rad)));}
+
+	/**
+	* @brief From wind speed and direction to v component (south-to-north)
+	* @param VW (wind speed)
+	* @param DW (wind direction)
+	* @return V component of wind.
+	*/
+	inline double VWDW_TO_V(const double& VW, const double& DW) {return ((VW==nodata || DW==nodata)? nodata : (- VW * cos(DW*Cst::to_rad)));}
+
 	/**
 	* @brief Check whether two values are equal regarding a certain epsilon environment (within certain radius of each other)
 	* @param val1
