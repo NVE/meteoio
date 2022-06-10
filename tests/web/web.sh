e9fdc2d49c028a8c01aa34da8aa858c4bc524679
Benjamin Bürgisser <benjamin.buergisser@slf.ch>
2022-06-10 15:30:12 +0200

    Web service test: use wget because curl has HTTP0.9 error

--- tests/web/web.sh
+++ tests/web/web.sh
@@ -3,9 +3,11 @@
 ../../bin/meteoio_timeseries_web -d "$(pwd)/jobs" -t 60 &
 RUNNING_PID=$!
 
-curl -v --location 'http://localhost:8080/wps' \
---header 'Content-Type: application/xml' \
---data-binary @webservice_test_input.xml
+RESULT=$(wget -qO- --post-file webservice_test_input.xml http://localhost:8080/wps)
+
+RESULT_LINK=$(echo $RESULT | grep -o -P '(?<=xlink:href=").*(?=")')
+
+wget "http://localhost:8080${RESULT_LINK}" -O /dev/null
 
 SUCCESS=$?
 
